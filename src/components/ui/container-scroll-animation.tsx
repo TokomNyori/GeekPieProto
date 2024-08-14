fa752ea6e2d2b072fb764a429bb3a96c7333058f
Tokom Nyori <tokom.nyori@gmail.com>
2024-08-14 23:52:53 +0530

    fixing window is not defined

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -17,6 +17,9 @@ export const ContainerScroll = ({
   const [isMobile, setIsMobile] = React.useState(false);
 
   React.useEffect(() => {
+    if (typeof window === 'undefined') {
+      return
+    }
     const checkMobile = () => {
       setIsMobile(window.innerWidth <= 768);
     };
