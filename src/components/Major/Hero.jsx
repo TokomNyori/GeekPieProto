f133025242cd089f2cdf5522860649f198272415
Tokom Nyori <tokom.nyori@gmail.com>
2024-08-15 04:14:06 +0530

    try

--- src/components/Major/Hero.jsx
+++ src/components/Major/Hero.jsx
@@ -43,7 +43,7 @@ const World = dynamic(() => import("../ui/globe").then((m) => m.World), {
 function Hero() {
   //dark:bg-grid-white/[0.2] bg-grid-black/[0.2]
   return (
-    <div className="h-auto md:h-screen w-full rounded-md flex flex-col lg:flex-row justify-between items-center lg:justify-between md:items-center overflow-hidden relative py-10 lg:py:0 ">
+    <div className="h-auto lg:h-screen w-full rounded-md flex flex-col-reverse lg:flex-row justify-between items-center lg:justify-between lg:items-center overflow-hidden relative py-10 lg:py:0 ">
       <ShootingStars />
       <StarsBackground />
       <Spotlight
@@ -51,7 +51,7 @@ function Hero() {
         fill="white"
       />
       {/* <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_-75%,black)]"></div> */}
-      <div className="mt-20 lg:mt-0 relative w-full z-10 p-4 px-6 lg:px-8 text-center lg:text-start lg:ml-10">
+      <div className="relative w-full z-10 p-4 px-6 lg:px-8 text-center lg:text-start mt-2 lg:ml-10 lg:mt-0">
         <TailwindConnectBtn text="Introducing GeekPie" className="mb-4" />
         <h1 className={clsx(`font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-500 text-4xl md:text-6xl py-1`)}>
           Cutting-Edge Apps. <br /> Stunning Design.
@@ -65,13 +65,13 @@ function Hero() {
           </Button>
         </Link>
       </div>
-      <div className="relative h-80 md:h-[100%] w-full">
+      <div className="relative h-80 md:h-[36rem] lg:h-[100%] w-full flex justify-center items-start mt-14 md:mt-0">
         {/* <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-20" /> */}
-        <div className="absolute w-full md:w-[95%] bottom-0 h-80 md:h-[95%] z-10">
+        <div className="absolute w-fit h-80 md:w-[95%] md:h-[95%] lg:w-[95%] lg:h-[95%] bottom-0 lg:bottom-2 z-10 p-0">
           <World data={sampleArcs} globeConfig={globeConfig} />
         </div>
       </div>
-      <div className="absolute z-30 bottom-5 md:bottom-10 text-center mx-auto w-full text-neutral-300">
+      <div className="absolute z-30 bottom-3 lg:bottom-10 text-center mx-auto w-full text-neutral-300">
         Backed by
         <IconCode className="inline mx-1" stroke={1} />
         Computer Science
