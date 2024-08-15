ba4e6ec9bb9e2b5b3e49874d6151549b4bb8a8af
Tokom Nyori <tokom.nyori@gmail.com>
2024-08-16 00:44:46 +0530

    added gemini effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import AdvantageScroll from "@/components/Major/AdvantageScroll";
 import Introduction from "@/components/Major/Introduction";
 import Services from "@/components/Major/Services";
 import TechIntegration from "@/components/Major/TechIntegration";
+import ChatBotIntro from "@/components/Major/ChatBotIntro";
 
 export default function Home() {
   return <main className="min-h-screen antialiased">
@@ -12,5 +13,9 @@ export default function Home() {
     <AdvantageScroll/>
     <TechIntegration/>
     <Services/>
+    <ChatBotIntro/>
+    <div className="h-[100rem] flex justify-center items-center text-3xl">
+      <div className="-mt-[87rem]">Services</div>
+    </div>
   </main>;
 }
