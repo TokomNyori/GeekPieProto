944e414791765f1f1c4c0aac7a09df9935cbc7b3
Tokom Nyori <tokom.nyori@gmail.com>
2024-08-17 01:13:20 +0530

    added Bento grid and feature section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
 import Image from "next/image";
-import Hero from "@/components/Major/Hero"
+import Hero from "@/components/Major/Hero";
 import AdvantageScroll from "@/components/Major/AdvantageScroll";
 import Introduction from "@/components/Major/Introduction";
 import Services from "@/components/Major/Services";
 import TechIntegration from "@/components/Major/TechIntegration";
 import ChatBotIntro from "@/components/Major/ChatBotIntro";
+import FeaturedSection from "@/components/Major/FeaturedSection";
 
 export default function Home() {
-  return <main className="min-h-screen antialiased">
-    <Hero/>
-    <Introduction/>
-    <AdvantageScroll/>
-    <TechIntegration/>
-    <Services/>
-    <ChatBotIntro/>
-    <div className="h-[100rem] flex justify-center items-center text-3xl">
-      <div className="-mt-[87rem]">Services</div>
-    </div>
-  </main>;
+  return (
+    <main className="min-h-screen antialiased">
+      <Hero />
+      <Introduction />
+      <AdvantageScroll />
+      <TechIntegration />
+      {/* <Services /> */}
+      <FeaturedSection />
+      <div className="h-[10rem] flex justify-center items-center text-3xl section">
+      </div>
+    </main>
+  );
 }
