eb82495154350330f80ef315b02022138e9e16d8
Tokom Nyori <tokom.nyori@gmail.com>
2024-09-15 19:34:13 +0530

    added modal 1

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,12 @@
+import React from "react";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Admin",
-  };
+  title: "Admin",
+};
+
+function Admin() {
+  return <div>page</div>;
+}
+
+export default Admin;
